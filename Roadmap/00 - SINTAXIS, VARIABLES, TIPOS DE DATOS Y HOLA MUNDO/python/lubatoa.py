edeaa792a0ecb03b78422961418e07ddfb82f5e9
lubatoa <168384734+lubatoa@users.noreply.github.com>
2024-04-28 22:51:18 -0500

    Update lubatoa.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lubatoa.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lubatoa.py
@@ -1,1 +1,22 @@
+#https://www.python.org/
 
+#comentario en una sola línea
+
+"""Comentario de 
+varias 
+líneas"""
+
+#creando variable
+
+my_variable = "primera" #variable
+
+MY_CONSTANT ="CONTANTE MITOLOGÍCA" #Contante en realidad no existe en Python
+
+my_int = 1 #entero
+my_float = 2.5 #flotante
+my_bool = false #booleano
+my_string = "Qué onda"
+
+#Imrimir en consola
+
+print("¡Hola, Python!")
